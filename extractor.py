1e701b349d200090f9096d35a83d32e48cc58adc
chatchapon <chatchapon.ryu@gmail.com>
2025-10-23 17:35:34 +0700

    add fps indicator before process video

--- extractor.py
+++ extractor.py
@@ -115,7 +115,8 @@ if __name__ == "__main__":
             logging.warning(f"'{str(vid_path)}' is not support video format")
             continue
 
-        logging.info(f"Process '{str(vid_path)}'")
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        logging.info(f"Process '{str(vid_path)}' | FPS: {fps}")
 
         frame_count = 0
         try:
